e02f4e5ce04fe1f4908cbf5b6102da7395d0fca1
Hưng Phan <phandinhhungvp2001@gmail.com>
2024-08-29 07:32:01 +0000

    support: Import SystemPropertyListPreference preference Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/mist/support/preferences/SystemPropertyListPreference.java
+++ src/com/mist/support/preferences/SystemPropertyListPreference.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2023-2024 the risingOS Project
+ * Copyright (C) 2023 The risingOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,40 +17,44 @@
 package com.mist.support.preferences;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.SystemProperties;
 import android.util.AttributeSet;
 
-import com.android.settingslib.development.SystemPropPoker;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 
-import com.mist.support.preferences.SelfRemovingListPreference;
+import com.android.internal.util.mist.MistUtils;
 
-public class SystemPropertyListPreference extends SelfRemovingListPreference {
+public class SystemPropertyListPreference extends ListPreference {
 
-    public SystemPropertyListPreference(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+    private static final String PREFS_NAME = "system_property_store_";
 
     public SystemPropertyListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    public SystemPropertyListPreference(Context context) {
-        super(context);
-    }
-
-    @Override
-    protected boolean isPersisted() {
-        return !SystemProperties.get(getKey(), "").isEmpty();
-    }
-
-    @Override
-    protected void putString(String key, String value) {
-        SystemProperties.set(key, value);
-        SystemPropPoker.getInstance().poke();
-    }
-
-    @Override
-    protected String getString(String key, String defaultValue) {
-        return SystemProperties.get(key, defaultValue);
+        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME + String.valueOf(getKey()), Context.MODE_PRIVATE);
+        String savedValue = preferences.getString(getKey(), null);
+        if (savedValue != null) {
+            setValue(savedValue);
+            setSummary("%s");
+        } else {
+            savedValue = SystemProperties.get(getKey());
+            if (savedValue != null && !savedValue.isEmpty()) {
+                setValue(savedValue);
+                setSummary("%s");
+            }
+        }
+        setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String value = (String) newValue;
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(getKey(), value);
+                editor.apply();
+                SystemProperties.set(getKey(), value);
+                MistUtils.showSystemUiRestartDialog(context);
+                return true;
+            }
+        });
     }
 }
